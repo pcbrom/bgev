ffe656f2da4502f5109801aefccbf3a35e14c470
pcbrom <pcbrom@gmail.com>
2022-01-26 13:58:36 -0300

    qq

--- R/qbgev.R
+++ R/qbgev.R
@@ -1,17 +1,20 @@
 #' Bimodal Generalized Extreme Value: Quantile Function
 #'
 #' @param p Probability.
-#' @param mu First location parameter.
+#' @param csi Shape parameter.
+#' @param mu Location parameter.
 #' @param sigma Scale parameter.
 #' @param delta Second location parameter.
 #' @param initial Starting point of range in desired quantile.
 #' @param final Starting point of range in desired quantile.
 #' @return Vector.
 #' @examples
-#' curve(pbgev(x, csi = 0, mu = 0, sigma = 1, delta = 2), xlim = c(-5, 5))
-#' (value <- qbgev(p = .25, csi = 0, mu = 0, sigma = 1, delta = 2))
-#' (value <- qbgev(p = .25, csi = 1, mu = 0, sigma = 1, delta = 2))
-#' pbgev(value, mu = -2, sigma = 1, delta = -1)
+#' curve(pbgev(x, csi = 1, mu = 2, sigma = 1, delta = 2), xlim = c(-5, 5))
+#' (value <- qbgev(p = .25, csi = 1, mu = 2, sigma = 1, delta = 2))
+#' pbgev(value, csi = 1, mu = 2, sigma = 1, delta = 2)
+#' curve(pbgev(x, csi = .1, mu = 2, sigma = 1, delta = 2), xlim = c(-5, 5))
+#' (value <- qbgev(p = .5, csi = .1, mu = 2, sigma = 1, delta = 2))
+#' pbgev(value, csi = .1, mu = 2, sigma = 1, delta = 2)
 #' @export
 
 qbgev <- function(p, csi, mu, sigma, delta) {
@@ -28,9 +31,6 @@ qbgev <- function(p, csi, mu, sigma, delta) {
     } else {
 
       yy <- ((-1)^((2 + delta) / (1 + delta))) * partial
-
-      # problema numerico:
-      # (-1)^((2 + delta) / (1 + delta))
 
     }
 
